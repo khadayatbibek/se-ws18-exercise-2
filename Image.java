77c566e0c8d845061c4dc94bfca896b52fdefdff
khadayat bibek <khadayat.bibek777@gmail.com>
2018-10-31 23:32:29 +0100

    Update Image.java
    updated image class(make setpixel   as void)

--- Image.java
+++ Image.java
@@ -1,59 +1,60 @@
 
+
 import java.awt.Color;
     
 
     import java.io.FileNotFoundException;
     import java.io.FileOutputStream;
     import java.io.IOException;
-    
+    import java.util.ArrayList;
 
 
 
 public class Image {
-  int height=0;
-  int width=0;
+	private static  int height=0;
+	private static  int width=0;
 	
 
-
+	private static ArrayList pic;
 	private static String matrix="";
 	
-	
+	private static Color color;
 	private final static String fileName="Assignment1.ppm";
 	
 	
-	public static void main(String[] args) throws IOException {
+	/*public static void main(String[] args) throws IOException {
 		
 		Image(200,200);
 	    write(fileName);	
 	
 		
 	}
-	
+	*/
 	
 	public static void Image(int width, int height){
-	
-		byte [] images=new byte[3];
+		pic=new ArrayList();
+		byte [] image=new byte[3];
 		matrix +="P3\n" + width + "\n" + height + "\n255\n";
 		
 		for (int i = 0; i < height; i++) {
 			for (int j = 0; j < width; j++) {
-				Color p= setPixels(width,height,j,i);
+				 setPixels(width,height,j,i);
 				
-				if(p==Color.red) {
-					images[0]=(byte)(255*factor(width,height,j,i));
-					images[1]=0;
-					images[2]=0;		
-				}else if (p==Color.green) {
-					images[0]=0;
-					images[1]=(byte)(255*factor(width,height,j,i));
-					images[2]=0;	
-				}else if (p==Color.blue) {
-					images[0]=0;
-					images[1]=0;
-					images[2]=(byte)(255*factor(width,height,j,i));
+				if(color==Color.red) {
+					image[0]=(byte)(255*factor(width,height,j,i));
+					image[1]=0;
+					image[2]=0;		
+				}else if (color==Color.green) {
+					image[0]=0;
+					image[1]=(byte)(255*factor(width,height,j,i));
+					image[2]=0;	
+				}else if (color==Color.blue) {
+					image[0]=0;
+					image[1]=0;
+					image[2]=(byte)(255*factor(width,height,j,i));
 					
 				}
-				matrix += ""+ images[0] + " " + images[1] + " " + images[2] + "  " ;
+				matrix += ""+ image[0] + " " + image[1] + " " + image[2] + "  " ;
 				
 				
 			}
@@ -64,15 +65,22 @@ public class Image {
 		
 	}
 	
-	public static Color setPixels(int width,int height, int a,int b ) {
+	public static void setPixels(int width,int height, int a,int b ) {
 			
 		double d1 = ((double) width / height) * a;
         double d2 = (((double) -width / height) * a + height);
-        if(d1 > b && d2 > b) return Color.green;
-        if(d1 > b && d2 < b) return Color.blue;
+        if(d1 > b && d2 > b) 
+        	{
+        	color= Color.green;
+        	}
+        else if(d1 > b && d2 < b)
+        {
+        	color= Color.blue;
+        	}
+        else {
        
-        return Color.red;
-
+        color= Color.red;
+        }
 	}
 	
 	
